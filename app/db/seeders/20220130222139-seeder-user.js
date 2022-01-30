e7161d3713ee121cdc78c88921eed609f1fa4a59
Taqin86 <rizqi.muttaqi@gmail.com>
2022-01-31 05:44:57 +0700

    seeder

---
+++ app/db/seeders/20220130222139-seeder-user.js
@@ -0,0 +1,35 @@
+'use strict';
+const bcrypt = require('bcrypt')
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    const password = bcrypt.hashSync('rahasia', 10)
+    await queryInterface.bulkInsert(
+      'Users', 
+      [
+        {
+          id: 1,
+          name: 'John Doe',
+          email: 'admin@gmail.com',
+          password: password,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          id: 2,
+          name: 'Elfin',
+          email: 'elfinn@gmail.com',
+          password: password,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+      ], 
+    {}
+    );
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.bulkDelete('Users', null, {});
+    
+  }
+};
